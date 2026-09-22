2ddf04c31bd7bd21b7e6f3f641b7ae89012a0466
Pavaki <pavaki190@gmail.com>
2026-09-22 10:00:42 -0600

    a

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hi! Welcome to Tic-Tac-Toe.")
+player_1=(input("Player one. Enter your name here."))
+player_1=(input("Player two. Enter your name here."))
+list=[
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+]
+print(list)
+print("Here is your tic-tac-toe board.")
