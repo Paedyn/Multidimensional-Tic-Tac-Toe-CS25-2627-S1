72c0847dd45cde674bb6c078d0b45504431b8b98
Pavaki <pavaki190@gmail.com>
2026-09-25 09:23:17 -0600

    A

--- part_1.py
+++ part_1.py
@@ -10,7 +10,7 @@ game_over = False
 print("Player 1 is X. Player 2 is O. To enter a value for the spot you want to place your symbol, you must enter the row number and column number. They are labelled 0, 1, 2.")
 
 while game_over == False:
-
+#so not game over while loop
     print("    0   1   2")
     print("  °❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･")
     print("0 |", board[0][0], "|", board[0][1], "|", board[0][2], "|")
@@ -18,23 +18,24 @@ while game_over == False:
     print("1 |", board[1][0], "|", board[1][1], "|", board[1][2], "|")
     print("  °❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･")
     print("2 |", board[2][0], "|", board[2][1], "|", board[2][2], "|")
-    print("  °❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･")
+    print("  °❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･")#columns
 
-    print(player, "turn")
+    print(f"{player}'s turn")
 
     row = int(input("Row (0-2): "))
     col = int(input("Column (0-2): "))
 
-    valid = True
+    good = True
 
     if row < 0 or row > 2 or col < 0 or col > 2:
         print("Invalid place! Try agai!")
-        valid = False
+       good = False
 
 
-    if valid == True:
+    if good == True:
         board[row][col] = player
         turns = turns + 1
+        #only 9 turns
 
         for r in range(3):
             if board[r][0] == player and board[r][1] == player and board[r][2] == player:
